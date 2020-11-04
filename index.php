84bb68066a768130e94cce2ebf6398ba75d965cb
fardin62 <70953546+fardin62@users.noreply.github.com>
2020-11-04 14:16:23 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
-<?php
-echo "Hello. Redirecting you to quiz site";
-sleep(5);
-?>
+<!DOCTYPE html>
 <html>
+  <h1>Wait, redirecting</h1>
 <script>
-window.location = "https://oceanian-ribbon.000webhostapp.com/quiz/quiz.php";
+//window.location = "https://oceanian-ribbon.000webhostapp.com/quiz/quiz.php";
 </script>
 </html>
