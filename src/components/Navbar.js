681d47a6e14224457d533b648a9d74fe8cca1d4e
Fardin Kamal <fardinkamal62@protonmail.ch>
2023-12-16 11:00:24 +0600

    Added menu on navbar, gets activated when screen size is small

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,12 @@
 
 import {AppBar, Box, Toolbar, Container, createTheme} from '@mui/material';
 import ThemeSwitch from '@/components/ThemeSwitch';
+import NavbarMenu from './NavbarMenu';
 
-const pages = [{ title: 'About', url: '#about' }, { title: 'Experience', url: '#experience' }, { title: 'Projects', url: '#projects' }, { title: 'Blogs', url: '#blogs' }];
+const pages = [{title: 'About', url: '#about'}, {title: 'Experience', url: '#experience'}, {
+    title: 'Projects',
+    url: '#projects'
+}, {title: 'Blogs', url: '#blogs'}];
 
 const navbarTheme = createTheme({
     components: {
@@ -19,21 +23,19 @@ const navbarTheme = createTheme({
 
 function NavBar() {
     return (
-        <AppBar position="sticky" className={'bg-gray-50 dark:bg-gray-950 dark:text-black'} sx={{boxShadow: 'none'}} theme={navbarTheme}>
+        <AppBar position="sticky" className={'bg-gray-50 dark:bg-gray-950 dark:text-black'} sx={{boxShadow: 'none'}}
+                theme={navbarTheme}>
             <Container maxWidth="xl">
                 <Toolbar className={'text-gray-700 dark:text-gray-400'}>
-                    <a href='/' className={`text-4xl`} style={{ textDecoration: 'none' }}>{'{~,~}'}</a>
-                    <Box sx={{ flexGrow: 100 }}></Box>
-                    {/* TODO On small screen hide the links and show a menu */}
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                    <a href='/' className={`text-4xl`} style={{textDecoration: 'none'}}>{'{~,~}'}</a>
+                    <Box sx={{flexGrow: 100}}></Box>
+                    <Box className={'flex sm:hidden md:flex'}>
                         {pages.map((page, index) => (
                             <a href={page.url} key={index} className={`text-lg mx-2 decoration-0`}>{page.title}</a>
                         ))}
                     </Box>
-
-                    <Box>
-                        <ThemeSwitch />
-                    </Box>
+                    <ThemeSwitch/>
+                    <NavbarMenu pages={pages}/>
                 </Toolbar>
             </Container>
         </AppBar>
---
+++ src/components/NavbarMenu.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import {IconButton, Menu, MenuItem} from '@mui/material';
+import {Menu as MenuIcon} from '@mui/icons-material';
+
+export default function NavbarMenu({pages}) {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div className={'flex md:hidden sm:flex'}>
+            <IconButton
+                aria-label="more"
+                id="long-button"
+                aria-controls={open ? 'long-menu' : undefined}
+                aria-expanded={open ? 'true' : undefined}
+                aria-haspopup="true"
+                onClick={handleClick}
+            >
+                <MenuIcon className={'dark:text-white'}/>
+            </IconButton>
+            <Menu
+                id="long-menu"
+                MenuListProps={{
+                    'aria-labelledby': 'long-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}>
+                {pages.map((page, index) => (
+                    <MenuItem key={index} onClick={handleClose}>
+                        <a href={page.url} key={index}>{page.title}</a>
+                    </MenuItem>
+                ))}
+            </Menu>
+        </div>
+    );
+}
