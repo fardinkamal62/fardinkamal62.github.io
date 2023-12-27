f3e9558c5dc6cce233b12941c06dd035f4164452
Fardin Kamal <fardinkamal62@protonmail.ch>
2023-12-27 09:08:35 +0600

    get project data from DB

--- src/app/projects/[title]/page.js
+++ src/app/projects/[title]/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import * as React from "react";
+import {useEffect, useState} from "react";
 
 import {Container} from '@mui/material';
 
@@ -11,15 +11,24 @@ import api from "@/util/api";
 
 
 export default function Projects({params}) {
-    api.post('/projects', { _key: 'project' }).then(res => console.log(res)).catch(err => console.log(err));
+    const {title} = params;
+
+    const [navbarPages, setNavbarPages] = useState([]);
+    const [project, setProject] = useState({});
+
+    useEffect(() => {
+        api.post('/', { _key: `project:${title}` }).then(res => {
+            setProject(res[0]);
+            setNavbarPages(res[0].navbar);
+        }).catch(err => console.log(err));
+    }, [title]);
     return (
         <>
             <NavBar pages={navbarPages}/>
             <Container maxWidth="xl" className='mt-20'>
                 <ScrollToTop/>
+                <div className={'mb-24'} dangerouslySetInnerHTML={{ __html: project.content }}/>
             </Container>
         </>
     )
 }
-
-const navbarPages = [];
