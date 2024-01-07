54ba9e2b5cc1b8236a129dbdb06a8f25b89bf2de
Fardin Kamal <fardinkamal62@protonmail.ch>
2024-01-07 20:46:04 +0600

    - added sharp package - added header content on backend

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -10,12 +10,14 @@ export async function POST(req) {
         return new Response(JSON.stringify(data), {
             status: 200,
             statusText: 'OK',
+            headers: { 'Content-Type': 'application/json' }
         })
     } catch (e) {
         console.log(e);
         return new Response(JSON.stringify(e), {
             status: 500,
             statusText: 'Internal Server Error',
+            headers: { 'Content-Type': 'application/json' }
         })
     }
 }
