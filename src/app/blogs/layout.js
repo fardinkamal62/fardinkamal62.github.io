aa1cddc17929c0ed683ba299cba2f68a15da78d6
Fardin Kamal <fardinkamal62@protonmail.ch>
2024-01-08 10:11:41 +0600

    fixed "You are mounting a new html component when a previous one has not first unmounted"

--- src/app/blogs/layout.js
+++ src/app/blogs/layout.js
@@ -7,12 +7,8 @@ export const metadata = {
 
 export default function RootLayout({children}) {
     return (
-        <html lang="en" suppressHydrationWarning>
-        <body>
         <ThemeProvider attribute={'class'}>
             {children}
         </ThemeProvider>
-        </body>
-        </html>
     )
 }
