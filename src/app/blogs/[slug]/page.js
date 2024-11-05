0eb53f92073bb8bc5d3bd554068700f8915267fc
Fardin Kamal <fardinkamal62@protonmail.ch>
2024-11-05 21:13:52 +0600

    Pluralized blog & project title

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -6,7 +6,7 @@ export async function generateMetadata({params}) {
 
     const project = await api.post('/', { _key: `blog:${slug}` })
     return {
-        title: `${project?.[0]?.title || 'Blog'} | Fardin Kamal`,
+        title: `${project?.[0]?.title || 'Blogs'} | Fardin Kamal`,
         description: project?.[0]?.description,
     }
 }
